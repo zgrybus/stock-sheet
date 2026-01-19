e4967b4d33204bf8b59dd58e11769e15413be4ef
Krzysztof Nofz <krzysztofnofz@gmail.com>
2026-01-19 20:11:47 +0100

    feat: add ktlint (#11)
    * Added ktlint
    
    * Updated lefthook.yml
    
    * added backend pr
    
    * Renamed package

--- src/main/kotlin/com/example/stocksheet/StockSheetApplication.kt
+++ src/main/kotlin/com/example/stocksheet/StockSheetApplication.kt
@@ -1,4 +1,4 @@
-package com.example.stock_sheet
+package com.example.stocksheet
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class StockSheetApplication
 
 fun main(args: Array<String>) {
-	runApplication<StockSheetApplication>(*args)
+    runApplication<StockSheetApplication>(*args)
 }
--- src/test/kotlin/com/example/stocksheet/StockSheetApplicationTests.kt
+++ src/test/kotlin/com/example/stocksheet/StockSheetApplicationTests.kt
@@ -1,13 +1,11 @@
-package com.example.stock_sheet
+package com.example.stocksheet
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class StockSheetApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {
+    }
 }
