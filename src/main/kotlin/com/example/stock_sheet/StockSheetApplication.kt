fb2de1f73176c97b590217befcae5e97432c18fb
Krzysztof Nofz <krzysztofnofz@gmail.com>
2026-01-19 19:40:41 +0100

    feat: init backend (#10)
    * Init backend code
    
    * Updated labeler

---
+++ src/main/kotlin/com/example/stock_sheet/StockSheetApplication.kt
@@ -0,0 +1,11 @@
+package com.example.stock_sheet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StockSheetApplication
+
+fun main(args: Array<String>) {
+	runApplication<StockSheetApplication>(*args)
+}
