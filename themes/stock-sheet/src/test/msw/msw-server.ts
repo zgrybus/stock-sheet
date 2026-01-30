9409d745c5727b84b346309f42c346d1e5eb3c0f
zgrybus <krzysztofnofz@gmail.com>
2026-01-30 13:29:52 +0100

    Added default handlers

--- themes/stock-sheet/src/test/msw/msw-server.ts
+++ themes/stock-sheet/src/test/msw/msw-server.ts
@@ -1,3 +1,12 @@
+import { $mswStockSheetApi } from "@/apis/stock-sheet/msw";
 import { setupServer } from "msw/node";
 
-export const mswServer = setupServer();
+export const mswServer = setupServer(
+  $mswStockSheetApi.get(
+    "/api/operations/portfolio/{currency}",
+    ({ response }) => response(200).json({ currency: "", positions: [] }),
+  ),
+  $mswStockSheetApi.post("/api/operations/import/{currency}", ({ response }) =>
+    response(200).json({ added: [], duplicated: [] }),
+  ),
+);
