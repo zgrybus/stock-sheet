8fd09ce81606c9f524e0deadeb2791b1e7892907
zgrybus <krzysztofnofz@gmail.com>
2026-02-09 22:10:04 +0100

    Added logic

--- themes/stock-sheet/src/routes/create-portfolio/index.tsx
+++ themes/stock-sheet/src/routes/create-portfolio/index.tsx
@@ -3,7 +3,12 @@ import { ArrowLeft, Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
-import { Field, FieldDescription, FieldLabel } from "@/components/ui/field";
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldLabel,
+} from "@/components/ui/field";
 import {
   Select,
   SelectContent,
@@ -11,12 +16,30 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { useForm } from "@tanstack/react-form";
+import { z } from "zod";
 
 export const Route = createFileRoute("/create-portfolio/")({
   component: RouteComponent,
 });
 
+const portfolioSchema = z.object({
+  name: z.string().min(1, "Nazwa jest wymagana"),
+  currency: z.string().min(1, "Wybierz walutę"),
+});
+
 function RouteComponent() {
+  const form = useForm({
+    defaultValues: {
+      name: "",
+      currency: "",
+    },
+    validators: {
+      onChange: portfolioSchema,
+    },
+    onSubmit: () => {},
+  });
+
   return (
     <div className="mx-auto max-w-5xl">
       <Link
@@ -48,51 +71,90 @@ function RouteComponent() {
           Zdefiniuj parametry swojego nowego portfela inwestycyjnego.
         </p>
       </section>
-      <Card>
-        <CardContent>
-          <Field>
-            <FieldLabel htmlFor="name">Nazwa portfela</FieldLabel>
-            <Input
-              id="name"
-              placeholder="np. Portfel Dywidendowy"
-              className={`border-muted-foreground/30`}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          form.handleSubmit();
+        }}
+      >
+        <Card>
+          <CardContent>
+            <form.Field
+              name="name"
+              children={(field) => (
+                <Field data-invalid={field.state.meta.errors.length > 0}>
+                  <FieldLabel htmlFor={field.name}>Nazwa portfela</FieldLabel>
+                  <Input
+                    id={field.name}
+                    value={field.state.value}
+                    onBlur={field.handleBlur}
+                    onChange={(e) => field.handleChange(e.target.value)}
+                    placeholder="np. Portfel Dywidendowy"
+                    className={`border-muted-foreground/30`}
+                    aria-invalid={field.state.meta.errors.length > 0}
+                  />
+                  <FieldDescription>
+                    Nazwa pomoże Ci odróżnić ten portfel w menu przełącznika.
+                  </FieldDescription>
+                  <FieldError errors={field.state.meta.errors} />
+                </Field>
+              )}
             />
 
-            <FieldDescription>
-              Nazwa pomoże Ci odróżnić ten portfel w menu przełącznika.
-            </FieldDescription>
-          </Field>
-          <Field className="mt-6">
-            <FieldLabel htmlFor="currency">Waluta bazowa</FieldLabel>
-            <Select>
-              <SelectTrigger
-                id="currency"
-                className={`border-muted-foreground/30`}
-              >
-                <SelectValue placeholder="Wybierz walutę" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="PLN">PLN - Złoty polski</SelectItem>
-                <SelectItem value="USD">USD - Dolar amerykański</SelectItem>
-                <SelectItem value="EUR">EUR - Euro</SelectItem>
-                <SelectItem value="GBP">GBP - Funt brytyjski</SelectItem>
-              </SelectContent>
-            </Select>
-            <FieldDescription>
-              Główna waluta, w której wyliczane będą statystyki portfela.
-            </FieldDescription>
-          </Field>
-        </CardContent>
-        <CardFooter className="mt-6 justify-end gap-3">
-          <Button variant="ghost" size="lg" asChild>
-            <Link to="/">Anuluj</Link>
-          </Button>
-          <Button size="lg">
-            <Plus className="size-4" />
-            Utwórz portfel
-          </Button>
-        </CardFooter>
-      </Card>
+            <form.Field
+              name="currency"
+              children={(field) => (
+                <Field
+                  className="mt-6"
+                  data-invalid={field.state.meta.errors.length > 0}
+                >
+                  <FieldLabel htmlFor={field.name}>Waluta bazowa</FieldLabel>
+                  <Select
+                    value={field.state.value}
+                    onValueChange={(value) => field.handleChange(value)}
+                  >
+                    <SelectTrigger
+                      id={field.name}
+                      aria-invalid={field.state.meta.errors.length > 0}
+                      className={`border-muted-foreground/30`}
+                    >
+                      <SelectValue placeholder="Wybierz walutę" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="PLN">PLN - Złoty polski</SelectItem>
+                      <SelectItem value="USD">
+                        USD - Dolar amerykański
+                      </SelectItem>
+                      <SelectItem value="EUR">EUR - Euro</SelectItem>
+                      <SelectItem value="GBP">GBP - Funt brytyjski</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FieldDescription>
+                    Główna waluta, w której wyliczane będą statystyki portfela.
+                  </FieldDescription>
+                  <FieldError errors={field.state.meta.errors} />
+                </Field>
+              )}
+            />
+          </CardContent>
+          <CardFooter className="mt-6 justify-end gap-3">
+            <Button variant="ghost" size="lg" asChild>
+              <Link to="/">Anuluj</Link>
+            </Button>
+
+            <form.Subscribe
+              selector={(state) => [state.isSubmitting]}
+              children={([isSubmitting]) => (
+                <Button type="submit" size="lg" loading={isSubmitting}>
+                  <Plus className="size-4" />
+                  Utwórz portfel
+                </Button>
+              )}
+            />
+          </CardFooter>
+        </Card>
+      </form>
     </div>
   );
 }
