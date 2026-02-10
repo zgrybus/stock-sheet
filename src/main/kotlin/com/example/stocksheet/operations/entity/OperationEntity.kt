cc2cc7077821f52afb41246f264478bb8460ce80
zgrybus <krzysztofnofz@gmail.com>
2026-02-10 22:04:13 +0100

    Added PorfolioEntity

--- src/main/kotlin/com/example/stocksheet/operations/entity/OperationEntity.kt
+++ src/main/kotlin/com/example/stocksheet/operations/entity/OperationEntity.kt
@@ -1,12 +1,16 @@
 package com.example.stocksheet.operations.entity
 
+import com.example.stocksheet.portfolio.entity.PortfolioEntity
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.EnumType
 import jakarta.persistence.Enumerated
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 import org.hibernate.annotations.CreationTimestamp
 import org.hibernate.annotations.UpdateTimestamp
@@ -36,6 +40,9 @@ class OperationEntity(
     var totalPrice: BigDecimal,
     @Column(nullable = false)
     var currency: String,
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "portfolio_id", nullable = false, updatable = false)
+    var portfolio: PortfolioEntity,
     @CreationTimestamp
     @Column(nullable = false, updatable = false)
     var createdAt: Instant? = null,
