433648c77ce11566daceb5632b9e98998c197d1d
zgrybus <krzysztofnofz@gmail.com>
2026-02-18 21:19:17 +0100

    Moved search param to root

--- themes/stock-sheet/src/routes/__root.tsx
+++ themes/stock-sheet/src/routes/__root.tsx
@@ -1,11 +1,17 @@
 import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
 import type { QueryClient } from "@tanstack/react-query";
+import z from "zod";
 
 type MyRouterContext = {
   queryClient: QueryClient;
 };
 
+const portfolioSearchParam = z.object({
+  portfolioId: z.number().optional(),
+});
+
 export const Route = createRootRouteWithContext<MyRouterContext>()({
+  validateSearch: portfolioSearchParam,
   component: RootComponent,
 });
 
--- themes/stock-sheet/src/routes/_app/_portfolio.tsx
+++ themes/stock-sheet/src/routes/_app/_portfolio.tsx
@@ -1,12 +1,6 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
-import z from "zod";
-
-const portfolioSearchParam = z.object({
-  portfolioId: z.number().optional(),
-});
 
 export const Route = createFileRoute("/_app/_portfolio")({
-  validateSearch: portfolioSearchParam,
   component: PortfolioGuardComponent,
   // TODO: add portfolio id, when id is not set in the search params
 });
