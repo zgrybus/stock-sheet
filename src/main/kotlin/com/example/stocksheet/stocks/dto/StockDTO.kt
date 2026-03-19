72a3820d291fd8b580629de33fa03379f31dfb77
zgrybus <krzysztofnofz@gmail.com>
2026-03-19 22:11:03 +0100

    MIgrated to StockEntity

---
+++ src/main/kotlin/com/example/stocksheet/stocks/dto/StockDTO.kt
@@ -0,0 +1,8 @@
+package com.example.stocksheet.stocks.dto
+
+data class StockDTO(
+    val name: String,
+    val symbol: String,
+    val exchange: String,
+    val industry: String,
+)
---
+++ src/main/kotlin/com/example/stocksheet/stocks/mappers/StockMapper.kt
@@ -0,0 +1,16 @@
+package com.example.stocksheet.stocks.mappers
+
+import com.example.stocksheet.stocks.dto.StockDTO
+import com.example.stocksheet.stocks.entity.StockEntity
+import org.springframework.stereotype.Component
+
+@Component
+class StockMapper {
+    fun toEntity(dto: StockDTO): StockEntity =
+        StockEntity(
+            name = dto.name,
+            symbol = dto.symbol,
+            exchange = dto.exchange,
+            industry = dto.industry,
+        )
+}
