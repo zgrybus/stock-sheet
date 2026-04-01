25cc24a8320f129f473cac82b6a3a8a1866457c9
zgrybus <krzysztofnofz@gmail.com>
2026-04-01 20:12:41 +0200

    Removed relation

--- src/main/kotlin/com/example/stocksheet/portfolio/entity/PortfolioEntity.kt
+++ src/main/kotlin/com/example/stocksheet/portfolio/entity/PortfolioEntity.kt
@@ -1,14 +1,10 @@
 package com.example.stocksheet.portfolio.entity
 
-import com.example.stocksheet.operations.entity.OperationEntity
-import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-import jakarta.persistence.OneToMany
 import jakarta.persistence.Table
 
 @Entity
@@ -21,6 +17,4 @@ class PortfolioEntity(
     var name: String,
     @Column(nullable = false)
     var currency: String,
-    @OneToMany(mappedBy = "portfolio", fetch = FetchType.LAZY, cascade = [CascadeType.ALL], orphanRemoval = true)
-    var operations: MutableList<OperationEntity> = mutableListOf(),
 )
